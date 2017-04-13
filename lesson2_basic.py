e2ec99f7d4aaead0b7f4f549d2335d4598b3f04b
Kacao Cao <kacao@iastate.edu>
2017-04-13 18:57:48 -0500

    Thursday work: pyqtGraph

---
+++ lesson2_basic.py
@@ -0,0 +1,36 @@
+import sys
+from PyQt4 import QtGui, Qtcore
+import pyqtgraph
+import numpy as np
+import time
+
+class Window(QtGui.QMainWindow):
+    def __init__(self):
+        super(Window, self).__init__
+        self.setGeometry(50,50,500,300)
+        self.setWindowTitle()
+        self.setWindowIcon(QtGui.QIcon('xxx.png'))
+
+    def home(self):
+        btn = Qtn.QPushButton("Quit", self)
+        btn.clicked.connect(self.close_application)
+        btn.resize(100,100)
+        btn.move(100,100)
+        self.show()
+
+    def set_frequency(self):
+        spin1 = Qtn.QSpinText("Frequency", freq_spin_1)
+        spin2 = Qtn.QSpinText("Amplifier", amp_spin_2)
+        spin3 = Qtn.QSpinText("Frequency", amp_spin_3)
+        spin4 = Qtn.QSpinText("Amplifier", amp_spin_4)
+
+    def QT_draph(self):
+
+    def update(self):
+        t1 = time.clock()
+        points = 100        #number of data points
+        X=np.arance(points)
+
+    def close_application(self):
+        print("Closing the application")
+        sys.exit()
---
+++ pyGraph.py
@@ -0,0 +1,41 @@
+import pyqtgraph as pg
+
+### create data to plot ######
+from numpy import *
+pi=3.1415
+X=linspace(-10,10,100)
+X1=linspace(-1,1,10);X11=linspace(-1,1,11)
+Y=2+sin(X1);Y1=2+sin(X);
+Y2=(cos(10*Y)/(X1+0.0131415))**2
+Y3=cos(10*Y1)/(X+0.0131415)
+Y4=4+sin(X)*cos(2*X)
+###############################
+
+##### plot two graphs in two windows ########
+pg.plot(X,Y1)
+# another graph
+pg.plot(X,Y1,pen=(255,0,255),symbol='+')
+# show
+pg.QtGui.QApplication.exec_()
+
+### plot two graphs on the same window ####
+H=pg.plot(X,Y1, pen='b',symbolPen="w",symbol='t')
+H.plot(X,Y1, pen=(255,0,0),symbolPen="r",symbol="s")
+H.plot(1.1*X,sqrt(Y1),symbolPen="y")
+# show
+pg.QtGui.QApplication.exec_()
+
+##### make a window with multiple graphs ####
+win= pg.GraphicsWindow(title="subplot window") # make the window
+p=win.addPlot(title="fig 1");p.plot(X,Y1)
+p=win.addPlot(title="fig 2");p.plot(X,Y4)
+# add axis labels
+p.setLabel('left','Y axis text',labelStyle = {'font-size': '48pt'})
+p.setLabel('bottom','X axis text',labelStyle = {'font-size': '48pt'})
+win.nextRow() # go down one row in the window
+p=win.addPlot(title="fig 3");p.plot(X,Y3)
+curve=pg.PlotCurveItem(X11,Y2,stepMode=True,fillLevel=0, brush=(0, 0, 255, 80))
+p=win.addPlot(title="fig 4");p.addItem(curve)
+
+# show
+pg.QtGui.QApplication.exec_()
